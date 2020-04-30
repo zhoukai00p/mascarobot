453aad2ad6f5e67ce5a74e512bccba1c141ea4ca
zhoukai00p <1041986332kai@gmail.com>
2020-04-30 07:46:50 +0200

    DisableEveryone

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ytdl = require('ytdl-core');
 const token = process.env.token;
 const prefix = process.env.prefix;
 
-const client = new Discord.Client();
+const client = new Discord.Client({ disableEveryone: true });
 
 client.once('ready', () => {
     console.log('Mascaro BOT online!');
@@ -21,10 +21,8 @@ client.once('ready', () => {
 
 
 client.on('message', message => {
-
-    if ((message.content === 'ciao') || (message.content === 'Ciao') || (message.content === 'Ciao masca') || (message.content === 'Ciao Masca') || 
-    (message.content === 'ciao masca') || (message.content === 'ciao Masca') || (message.content === 'Ciao Mascaro') || (message.content === 'ciao Mascaro') || 
-    (message.content === 'ciao mascaro') || (message.content === 'Ciao mascaro'))
+    var messaggio = message.content.toLocaleLowerCase();
+    if ((messaggio === 'ciao masca') || (messaggio === 'ciao mascaro') || (messaggio === 'ciao mascarobot') || (messaggio === 'ciao mascaro bot'))
     {
 
        message.reply(' Ciao!');
